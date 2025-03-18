38932c1df5a10d0a5f44e5c7d621aaa914012367
Louis Aurea <louis@nextstation.cl>
2025-03-17 20:57:19 -0400

    adding mongoDb setup

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import compression from 'compression';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-
 
 app.use(cors({
     credentials: true,
@@ -16,10 +17,17 @@ app.use(cors({
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
- 
 const server = http.createServer(app);
 
 server.listen(process.env.PORT, () => {
     console.log("listenting port https://localhost:3000");
 });
+const MONGODB_URI = process.env.MONGODB_URI;
 
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        console.log('Conectado a MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error al conectar a MongoDB:', error);
+    });
