04236ff51be836bf98640b598c295dc727c4fbac
Louis Aurea <louis@nextstation.cl>
2025-03-17 20:38:32 -0400

    add port 3000

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello recruiter");
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import compression from 'compression';
+
+
+const app = express();
+
+
+app.use(cors({
+    credentials: true,
+
+}));
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+ 
+const server = http.createServer(app);
+
+server.listen(process.env.PORT, () => {
+    console.log("listenting port https://localhost:3000");
+});
+
